bdafc0fe56db22247eb73dfea5957a6ece731c97
vitol <omardvilko@mail.ru>
2017-09-26 17:43:26 +0300

    Product my

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function multiply(first, second) {
-  // your solution
+		var arr1 = x.split(''); // разбиваем строку в массив по одному элементу = цифра числа
+		var arr2 = y.split('');
+		var arrNum1 = arr1.map(Number).reverse(); // преобразуем в числовой массив и переворачиваем
+		var arrNum2 = arr2.map(Number).reverse();
+		var newArr =[];
+		
+		for (i = 0; i < arrNum1.length; i++) {  		// переумножаем цифры и добавляем в новый массив, складывая по разрядам
+			for (j = 0; j < arrNum2.length; j++) {
+				var x = newArr[i+j] || 0;  		// определяем предыдущее число нужного индекса массива 
+				newArr[i+j] = (arrNum2[j]*arrNum1[i] + x);
+			}
+		}
+		
+		for (i = 0; i < newArr.length; i++) {  		// избавляемся от лишних разрядов и суммируем числа
+			var x = Math.floor(newArr[i] / 10);
+			newArr[i] = newArr[i] % 10;
+			if (x != 0) {
+				if (newArr[i+1]) {
+					newArr[i+1] = newArr[i+1] + x;
+				} else {
+					newArr[i+1] = x;
+				}
+			}
+		}
+		
+		return result = newArr.reverse().join(''); //преворачиваем массив обратно и преобразум в строку 
+		
 }
